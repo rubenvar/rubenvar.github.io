74853e644e86184ef053f07050ac9344c2e2b714
Rubén <hey@rubenvara.io>
2020-12-15 03:55:42 +0100

    style cards, add npm registry api call

---
+++ scripts.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const npmTitle = document.getElementById('npm-title');
+const npmContainer = document.getElementById('npm-packages');
+
+function addToPage(arr) {
+  let result = '';
+  arr.forEach(obj => result += `<p><a href="${obj.package.links.npm}">${obj.package.name}</a></p>`);
+  return npmContainer.innerHTML = result;
+}
+
+axios
+.get('https://registry.npmjs.org/-/v1/search?text=maintainer:rubenvara')
+.then(res => {
+  if (res.data.total) {
+    npmTitle.innerHTML = res.data.total;
+    return addToPage(res.data.objects);
+  }
+})
+.catch(err => console.error(err))
