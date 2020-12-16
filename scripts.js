f2080f194c3a3cc9ee385aa52ba5a2652977f1d0
Rubén <hey@rubenvara.io>
2020-12-16 02:30:29 +0100

    add projects, style, hide/show tech part, toggle view

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import { doc } from 'prettier';
 
 const npmTitle = document.getElementById('npm-title');
 const npmContainer = document.getElementById('npm-packages');
 
+// render npm registry data to document
 function addToPage(arr) {
   let result = '';
   arr.forEach(
@@ -12,6 +14,7 @@ function addToPage(arr) {
   return (npmContainer.innerHTML = result);
 }
 
+// get npm packages data from the registry
 if (npmTitle && npmContainer) {
   axios
     .get('https://registry.npmjs.org/-/v1/search?text=maintainer:rubenvara')
@@ -23,3 +26,28 @@ if (npmTitle && npmContainer) {
     })
     .catch((err) => console.error(err));
 }
+
+// show/hide tech info in cards
+const tech = document.getElementsByClassName('tech');
+function showHiddenTech(e) {
+  if (e.target.className === 'boss') {
+    e.target.parentNode.classList.remove('hide');
+    e.target.hidden = true;
+  }
+}
+
+[...tech].forEach((div) => div.addEventListener('click', showHiddenTech));
+
+// toggle list/grid view
+const viewToggler = document.getElementById('view');
+const projects = document.getElementById('projects');
+let next = 'grid';
+function toggleView(e) {
+  const n = next;
+  const o = n === 'list' ? 'grid' : 'list';
+  projects.classList.add(n);
+  projects.classList.remove(o);
+  next = o;
+}
+
+viewToggler.addEventListener('click', toggleView);
