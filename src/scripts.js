8377f3dc5e0efd2e272c1cab9f3026a1f6e1ba20
Rubén <hey@rubenvara.io>
2022-02-14 13:17:39 +0100

    comment npm querying scripts

--- src/scripts.js
+++ src/scripts.js
@@ -1,30 +1,31 @@
-import axios from 'axios';
+// ? not enabled for now:
+// import axios from 'axios';
 
-const npmTitle = document.getElementById('npm-title');
-const npmContainer = document.getElementById('npm-packages');
+// const npmTitle = document.getElementById('npm-title');
+// const npmContainer = document.getElementById('npm-packages');
 
-// render npm registry data to document
-function addToPage(arr) {
-  let result = '';
-  arr.forEach(
-    (obj) =>
-      (result += `<p><a href="${obj.package.links.npm}">${obj.package.name}</a></p>`)
-  );
-  return (npmContainer.innerHTML = result);
-}
+// // render npm registry data to document
+// function addToPage(arr) {
+//   let result = '';
+//   arr.forEach(
+//     (obj) =>
+//       (result += `<p><a href="${obj.package.links.npm}">${obj.package.name}</a></p>`)
+//   );
+//   return (npmContainer.innerHTML = result);
+// }
 
-// get npm packages data from the registry
-if (npmTitle && npmContainer) {
-  axios
-    .get('https://registry.npmjs.org/-/v1/search?text=maintainer:rubenvara')
-    .then((res) => {
-      if (res.data.total) {
-        npmTitle.innerHTML = res.data.total;
-        return addToPage(res.data.objects);
-      }
-    })
-    .catch((err) => console.error(err));
-}
+// // get npm packages data from the registry
+// if (npmTitle && npmContainer) {
+//   axios
+//     .get('https://registry.npmjs.org/-/v1/search?text=maintainer:rubenvara')
+//     .then((res) => {
+//       if (res.data.total) {
+//         npmTitle.innerHTML = res.data.total;
+//         return addToPage(res.data.objects);
+//       }
+//     })
+//     .catch((err) => console.error(err));
+// }
 
 // show/hide tech info in cards
 const techTogglers = document.getElementsByClassName('tech-toggler');
